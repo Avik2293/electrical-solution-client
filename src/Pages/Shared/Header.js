ad221b392ca68908370a556d87ecb129843c323f
Avik2293 <aviksarker22@gmail.com>
2022-11-08 21:03:34 +0600

    button toggle

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -12,8 +12,8 @@ const Header = () => {
 
     const handleLogOut = () => {
         logOut()
-        .then( () => {})
-        .catch( error => console.error(error))
+            .then(() => { })
+            .catch(error => console.error(error))
     };
 
     return (
@@ -25,8 +25,13 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to='/courses'>Courses</Link></li>
-                            <li><Link to='/faq'>FAQ</Link></li>
+                            {
+                                user?.uid &&
+                                <>
+                                    <li><Link to='/'>My Reviews</Link></li>
+                                    <li><Link to='/'>Add Service</Link></li>
+                                </>
+                            }
                             <li><Link to='/blog'>Blog</Link></li>
                         </ul>
                     </div>
@@ -34,8 +39,13 @@ const Header = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><Link to='/courses'>Courses</Link></li>
-                        <li><Link to='/faq'>FAQ</Link></li>
+                        {
+                            user?.uid &&
+                            <>
+                                <li><Link to='/'>My Reviews</Link></li>
+                                <li><Link to='/'>Add Service</Link></li>
+                            </>
+                        }
                         <li><Link to='/blog'>Blog</Link></li>
                     </ul>
                 </div>
